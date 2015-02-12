bd10774ee908cd3d4385cc070f97ff87e2d31300
HybridFox <hybridfox2@gmail.com>
2015-02-12 22:01:04 +0100

    Added item description

--- content/game.php
+++ content/game.php
@@ -2,7 +2,7 @@
   <div id="inventory">
     <div class="inventory-container">
       <div class="slot o-1">
-        
+
       </div>
       <div class="slot o-2">
 
@@ -24,8 +24,11 @@
     </div>
   </div>
   <div id="overlay">
-    <div class="pickup-item" data-item="test-sword" style="left: 50%;">
+    <div class="pickup-item" data-item="test-sword" data-name="Test Sword" style="left: 50%;">
       <img src="assets/img/game/items/test-sword.png" alt="Test Sword" class="hover"/>
+    </div>
+    <div class="pickup-item" data-item="test-salad" data-name="Test Salad \o/" style="left: 10%;">
+      <img src="assets/img/game/items/test-salad.png" alt="Test Salad" class="hover"/>
     </div>
   </div>
 </div>
