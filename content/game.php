c2a942e6e8dda8462f56ceea2abf3c49ed6dc528
HybridFox <hybridfox2@gmail.com>
2015-02-12 14:02:23 +0100

    Item in slots

--- content/game.php
+++ content/game.php
@@ -2,7 +2,9 @@
   <div id="inventory">
     <div class="inventory-container">
       <div class="slot">
-
+        <div class="item">
+          <img src="assets/img/game/items/test-sword.png" alt="Test" />
+        </div>
       </div>
       <div class="slot">
 
