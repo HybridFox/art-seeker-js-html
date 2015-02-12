93b3bfe3ac77a4093be43096fb7cc209eed9b0d1
HybridFox <hybridfox2@gmail.com>
2015-02-12 14:27:04 +0100

    Working hero chooser

--- assets/js/menu/menu.js
+++ assets/js/menu/menu.js
@@ -71,27 +71,76 @@ function loadGame(char) {
       $("#load").removeClass("animated zoomInUp");
       $("#load").addClass("animated bounceOut");
       clearInterval(loadbar);
-      gameInit();
+      gameInit(char);
     }
   }, 20)
 }
 
-function gameInit() {
+function gameInit(char) {
   $.get("content/game.php", function(data){
     $("#game").html(data);
     $("#game").addClass("animated zoomIn");
 
-    $("#character").animateSprite({
-    fps: 4,
-    loop: true,
-    animations: {
-      walkRight: [1, 2, 3, 4],
-      walkLeft: [6, 7, 8, 9],
-      idle: [0, 5]
-    },
-    complete: function(){
-      alert('Sprite animation complete!');
+  if (char == "hero1") {
+      $("#character").animateSprite({
+        fps: 4,
+        loop: true,
+        animations: {
+          walkRight: [1, 2, 3, 4],
+          walkLeft: [6, 7, 8, 9]
+        },
+        complete: function(){
+          alert('Sprite animation complete!');
+        }
+      });
+    } else if (char == "hero2") {
+      $("#character").animateSprite({
+        fps: 4,
+        loop: true,
+        animations: {
+          walkRight: [11, 12, 13, 14],
+          walkLeft: [16, 17, 18, 19]
+        },
+        complete: function(){
+          alert('Sprite animation complete!');
+        }
+      });
+    } else if (char == "hero3") {
+      $("#character").animateSprite({
+        fps: 4,
+        loop: true,
+        animations: {
+          walkRight: [21, 22, 23, 24],
+          walkLeft: [26, 27, 28, 29]
+        },
+        complete: function(){
+          alert('Sprite animation complete!');
+        }
+      });
+    } else if (char == "hero4") {
+      $("#character").animateSprite({
+        fps: 4,
+        loop: true,
+        animations: {
+          walkRight: [31, 32, 33, 34],
+          walkLeft: [36, 37, 38, 39]
+        },
+        complete: function(){
+          alert('Sprite animation complete!');
+        }
+      });
+    } else if (char == "hero5") {
+      $("#character").animateSprite({
+        fps: 4,
+        loop: true,
+        animations: {
+          walkRight: [41, 42, 43, 44],
+          walkLeft: [46, 47, 48, 49]
+        },
+        complete: function(){
+          alert('Sprite animation complete!');
+        }
+      });
     }
-  });
   });
 }
